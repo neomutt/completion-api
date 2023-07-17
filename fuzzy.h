4e36f8f79b249635896fbba783cd875e00280b06
Simon Reichel <simonreichel@giese-optik.de>
2023-07-17 12:42:05 +0100

    Update fuzzy header

--- fuzzy.h
+++ fuzzy.h
@@ -4,12 +4,15 @@
 #include "mutt/string2.h"
 #include "completion.h"
 
+// TODO might want to move these to mutt/mbyte.h or mutt/string2.h
 #define ISLONGMBYTE(mbyte) mblen(mbyte, MB_CUR_MAX) > 1
 #define MBCHARLEN(mbyte) mblen(mbyte, MB_CUR_MAX)
 #define ISBADMBYTE(mbyte) mblen(mbyte, MB_CUR_MAX) == -1
 
+bool is_mbs(const char *str);
 int mbs_char_count(const char *str);
 bool mb_equal(const char *stra, const char *strb);
+
 // TODO add fuzzy match function (could be reused for fuzzy finding in pager etc)
 int dist_lev(const char *stra, const char *strb);
-int dist_dam_lev(const char *stra, const char *strb, const struct Completion *comp);
+int dist_dam_lev(const char *stra, const char *strb, const Completion *comp);
